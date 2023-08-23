2300916aa1753bee2c38deeaa49873ef8709dffe
SpaceBa13 <brayanalpizar36@gmail.com>
2023-08-22 19:35:23 -0600

    Creacion Clase Cliente

--- src/main/java/com/mycompany/datosextraclase/Cliente.java
+++ src/main/java/com/mycompany/datosextraclase/Cliente.java
@@ -1,14 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.datosextraclase;
-
 /**
  *
  * @author SpaceBa
  */
-public class DatosExtraClase {
+public class Cliente {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
