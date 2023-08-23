b3735caef60cc59b8e42959d9eb5574a403ecab1
SpaceBa13 <brayanalpizar36@gmail.com>
2023-08-22 19:40:56 -0600

    Creacion del socket cliente

--- src/main/java/com/mycompany/datosextraclase/Cliente.java
+++ src/main/java/com/mycompany/datosextraclase/Cliente.java
@@ -1,11 +1,38 @@
 package com.mycompany.datosextraclase;
+
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author SpaceBa
  */
-public class Cliente {
+public class Cliente implements Runnable{
+    int puerto;
+    String mensaje;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public Cliente(int puerto, String mensaje){
+        this.puerto = puerto;
+        this.mensaje = mensaje;
+    }
+
+
+    @Override
+    public void run() {
+        String IP = "127.0.0.1";
+        DataOutputStream envio;
+        try{
+            Socket socket_c = new Socket(IP, puerto);
+            envio = new DataOutputStream(socket_c.getOutputStream());
+            envio.writeUTF(mensaje);
+            socket_c.close();
+
+        }catch (Exception ex){
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE,null,ex);
+
+        }
     }
 }
