2b22867350a14d5d2342557603b883d5eaee1ef1
SpaceBa13 <brayanalpizar36@gmail.com>
2023-08-22 19:30:55 -0600

    first commit

---
+++ src/main/java/com/mycompany/datosextraclase/DatosExtraClase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datosextraclase;
+
+/**
+ *
+ * @author SpaceBa
+ */
+public class DatosExtraClase {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
