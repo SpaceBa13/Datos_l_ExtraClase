c23c6b4cf12df482a1228b19fede10343cecb7d2
SpaceBa13 <brayanalpizar36@gmail.com>
2023-08-22 19:53:27 -0600

    Creacion del socket de parte del servidor y uso de observables

--- src/main/java/com/mycompany/datosextraclase/Servidor.java
+++ src/main/java/com/mycompany/datosextraclase/Servidor.java
@@ -1,6 +1,12 @@
 package com.mycompany.datosextraclase;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Observable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Servidor extends Observable implements Runnable{
     int puerto;
@@ -10,6 +16,27 @@ public class Servidor extends Observable implements Runnable{
 
     @Override
     public void run() {
+        ServerSocket servidor = null;
+        Socket cliente = null;
+        DataInputStream recibir;
+        String mensaje;
 
+        try{
+            servidor = new ServerSocket(puerto);
+            while(true){
+                cliente = servidor.accept();
+                recibir = new DataInputStream(cliente.getInputStream());
+                mensaje = recibir.readUTF();
+
+                this.setChanged();
+                this.notifyObservers(mensaje);
+                this.clearChanged();
+
+                cliente.close();
+            }
+
+        }catch (IOException ex){
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
